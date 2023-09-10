947fc1e3d31b79643ac6f1b311d911d8fc00a8ed
Hammad Imran <130256040+maddyexx@users.noreply.github.com>
2023-09-10 10:43:01 -0700

    Lecture10_Material

---
+++ Lecture10_Module/main.py
@@ -0,0 +1,10 @@
+# main.py
+
+import math_operations as mo
+import multiplication as ml
+result1 = mo.add(5, 3)
+result2 = mo.subtract(10, 4)
+result3 = ml.mul(5,10)
+print(result1)  # Output: 8
+print(result2)  # Output: 6
+print(result3)
---
+++ Lecture10_Module/math_operations.py
@@ -0,0 +1,7 @@
+# math_operations.py
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
