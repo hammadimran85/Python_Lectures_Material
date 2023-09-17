aee43b3539951f8afa0c21be57e1bb6e25d457e9
Hammad Imran <130256040+maddyexx@users.noreply.github.com>
2023-09-17 13:11:35 -0700

    Lecture Material

---
+++ Lecture13_Functions/Function_Class_Practice.py
@@ -0,0 +1,84 @@
+address_book = {}
+
+
+def add_contact():
+    name = str(input('Enter contact name').lower())
+    phone_number = int(input('Enter your phone number'))
+    address = str(input('Enter contact address'))
+    address_book[name] = {'name': name, 'phone_number': phone_number, 'address': address}
+    print(f'{name} successfully stored in address book')
+
+
+def display_contacts():
+    if not address_book:
+        print('Address book is empty')
+    else:
+        print('{:_^10}'.format('All Contacts'))
+        for index, name in enumerate(address_book.keys(), start=1):
+            print(f'{index}: {name}')
+
+
+def search_contact():
+    name = str(input('Enter the contact name to search its info').lower())
+    if name in address_book:
+        print('Contact Details:')
+        print(f'Name: {name}')
+        print(f'Contact Number: ', address_book[name]['phone_number'])
+        print(f'Address: ', address_book[name]['address'])
+    else:
+        print('The name is not in found address book')
+
+
+def delete_contact():
+    name = str(input('Enter the contact name to delete its info').lower())
+    if not address_book:
+        print('Address book is empty')
+    else:
+        if name in address_book:
+            del address_book[name]
+            print(f'{name} is no more in the address book')
+        else:
+            print(f'{name} does not exist in the address book')
+
+
+def update_contact():
+    name = str(input('Enter the contact name to delete its info').lower())
+    if not address_book:
+        print('Address book is empty')
+    else:
+        if name in address_book:
+            phone = input('Enter the updated phone number (Press Enter if you dont want to update number)')
+            address = input('Enter the updated address number (Press Enter if you dont want to update address)')
+            if phone:
+                address_book[name]['phone_number'] = phone
+            if address:
+                address_book[name]['address'] = address
+            print(f'{name} is updated in the address book')
+        else:
+            print(f'{name} does not exist in the address book')
+
+
+while True:
+    print('*********Address Book***********')
+    print('Press 1 to add a contact to book')
+    print('Press 2 to display all contacts')
+    print('Press 3 to search a contact in address book')
+    print('Press 4 to delete a contact from address book')
+    print('Press 5 to update a contact')
+    print('Press 6 to exit address book')
+
+    choice = int(input('Select yout choice: '))
+
+    if choice == 1:
+        add_contact()
+    elif choice == 2:
+        display_contacts()
+    elif choice == 3:
+        search_contact()
+    elif choice == 4:
+        delete_contact()
+    elif choice == 5:
+        update_contact()
+    elif choice == 6:
+        print('Thanks for using address book app')
+        break
